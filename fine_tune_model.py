6bbb4b5ad879031ce0aed0d07b215dcb4d5c595b
antonislouca <alouka04@cs.ucy.ac.cy>
2023-11-26 23:54:39 +0200

    updates

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -2,26 +2,51 @@ from getpass import getpass
 from langchain.llms import HuggingFaceHub
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.chains import SequentialChain
+import pickle
 
-HUGGINGFACEHUB_API_TOKEN = getpass()
+# HUGGINGFACEHUB_API_TOKEN = getpass()
 import os
+def read_list():
+    # for reading also binary mode is important
+    with open('/data/urlcontent.pickle', 'rb') as fp:
+        n_list = pickle.load(fp)
+        return n_list
+    
+# documents=read_list()
+# document_string=' '.join(str(v) for v in documents)
+    
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = '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'
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+question = "What is fogify?"
 
-question = "Who won the FIFA World Cup in the year 1994? "
+template = """
+Some information about the fogify tool: 
+Fogify is an emulation Framework easing the modeling, deployment 
+and experimentation of fog testbeds. 
+Fogify provides a toolset to: model complex 
+fog topologies comprised of heterogeneous resources,
+network capabilities and QoS criteria; deploy the modelled
+configuration and services using popular containerized 
+infrastructure-as-code descriptions to a cloud or local
+environment; experiment, measure and evaluate the deployment 
+by injecting faults and adapting the configuration at runtime 
+to test different "what-if" scenarios that reveal the
+limitations of a service before introduced to the public.
 
-template = """Question: {question}
+Question: {question}
 
-Answer: Let's think step by step."""
+Answer: """
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
+prompt = PromptTemplate(input_variables=[ 'question'],template=template)
 repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
 llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64}
+    repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length":500}
 )
-llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-print(llm_chain.run(question))
+llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
+
+print(llm_chain.run({ 'question':question}))
 
 # import pickle
 # def read_list():
