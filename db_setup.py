31c62f3fac2a550f7d5f01c1dc9649822204f3ab
antonislouca <antonis.loucaa@gmail.com>
2023-11-26 12:17:39 +0200

    db setup

--- db_setup.py
+++ db_setup.py
@@ -3,23 +3,26 @@ import os
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import text
-from langchain.vectorstores import Milvus
+# from langchain.vectorstores import Milvus
+from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 
 os.environ["OPENAI_API_KEY"] = "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"
 
 
-loader = TextLoader("url_content.txt")
+loader = TextLoader("/data/url_content.txt")
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
-vector_db = Milvus.from_documents(
-    docs,
-    embeddings,
-    connection_args={"host": "127.0.0.1", "port": "19530"},
-)
+db=Chroma.from_documents(docs,embeddings)
 
-docs=vector_db.similarity_search('Fogify')
-print(docs[0])
+# vector_db = Milvus.from_documents(
+#     docs,
+#     embeddings,
+#     connection_args={"host": "127.0.0.1", "port": "19530"},
+# )
+
+docs=db.similarity_search('Fogify')
+print(docs[0].page_content)
