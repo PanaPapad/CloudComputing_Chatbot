fe4e6e3ad9b2cfd8c09cb5c90975ecc667d14783
antonislouca <antonis.loucaa@gmail.com>
2023-11-27 16:22:40 +0200

    this does not work properly yet

---
+++ chat.py
@@ -0,0 +1,115 @@
+from getpass import getpass
+from langchain.llms import HuggingFaceHub
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SequentialChain
+import pickle
+from langchain.embeddings import SentenceTransformerEmbeddings
+
+from langchain.vectorstores import Chroma
+import warnings
+import os
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = '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'
+def read_list():
+    # for reading also binary mode is important
+    with open('/data/urlcontent.pickle', 'rb') as fp:
+        n_list = pickle.load(fp)
+        return n_list
+    
+def read_and_return_DB():
+    persist_directory = '/data/CHROMA_DB'
+    
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    vector_db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)  
+    return vector_db
+
+
+
+
+# with warnings.catch_warnings():
+    
+    
+def get_relevant_context (user_input,vector_db):None
+def setup():
+    question = "What is fogify?"
+
+    template = """
+    Some information about the fogify tool: 
+    Fogify is an emulation Framework easing the modeling, deployment 
+    and experimentation of fog testbeds. 
+    Fogify provides a toolset to: model complex 
+    fog topologies comprised of heterogeneous resources,
+    network capabilities and QoS criteria; deploy the modelled
+    configuration and services using popular containerized 
+    infrastructure-as-code descriptions to a cloud or local
+    environment; experiment, measure and evaluate the deployment 
+    by injecting faults and adapting the configuration at runtime 
+    to test different "what-if" scenarios that reveal the
+    limitations of a service before introduced to the public.
+
+    Question: {question}
+
+    Answer: """
+
+    prompt = PromptTemplate(input_variables=[ 'question'],template=template)
+    repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+    llm = HuggingFaceHub(
+        repo_id=repo_id, model_kwargs={"temperature": 1.0, "max_length":500}
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
+
+    print(llm_chain.run({ 'question':question}))
+# def create_prompt(question):
+#     question = "What is fogify?"
+#     #set up the template of the prompt
+#     template = """
+#     Some information about the fogify tool: 
+#     Fogify is an emulation Framework easing the modeling, deployment 
+#     and experimentation of fog testbeds. 
+#     Fogify provides a toolset to: model complex 
+#     fog topologies comprised of heterogeneous resources,
+#     network capabilities and QoS criteria; deploy the modelled
+#     configuration and services using popular containerized 
+#     infrastructure-as-code descriptions to a cloud or local
+#     environment; experiment, measure and evaluate the deployment 
+#     by injecting faults and adapting the configuration at runtime 
+#     to test different "what-if" scenarios that reveal the
+#     limitations of a service before introduced to the public.
+    
+#     Question: {question}
+    
+#     Answer: """
+#     #add the variables of the prompt
+#     prompt = PromptTemplate(input_variables=[ 'question'],template=template)
+#     repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+#     llm = HuggingFaceHub(
+#         repo_id=repo_id, model_kwargs={"temperature": 1.0, "max_length":500}
+#     )
+    
+#     #create the llm chain 
+#     llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
+#     #run the prompt by providing values fort the variables
+#     print( llm_chain.run({'question':question}))
+#     return llm_chain.run({'question':question})
+    
+def main():
+    print("============\nFogify chatbot\n============")
+    print("Fogify Chatbot: Hello, how can i help. You can type \"exit\" to exit the chat or ask any question about Fogify")
+    
+    #read database
+    vector_db=read_and_return_DB()
+    warnings.simplefilter("ignore")
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() == 'exit':
+            break
+        # context = get_relevant_context(user_input, vector_db)
+        # response = get_openai_response(user_input, context)
+        # response=create_prompt(user_input)
+        setup()
+        # print("Fogify Chatbot:", response)
+
+if __name__ == "__main__":
+    main()
