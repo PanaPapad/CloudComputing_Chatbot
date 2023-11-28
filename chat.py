5c2052d9f0aad5d3b69db3393a01a7550f9b6357
PanaPapad <126814800+PanaPapad@users.noreply.github.com>
2023-11-28 14:06:27 +0200

    working?

--- chat.py
+++ chat.py
@@ -5,12 +5,37 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import SequentialChain
 import pickle
 from langchain.embeddings import SentenceTransformerEmbeddings
-
 from langchain.vectorstores import Chroma
 import warnings
 import os
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = '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'
+template = """
+Some information about the fogify tool: 
+Fogify is an emulation Framework easing the modeling, deployment 
+and experimentation of fog testbeds. 
+Fogify provides a toolset to: model complex 
+fog topologies comprised of heterogeneous resources,
+network capabilities and QoS criteria; deploy the modelled
+configuration and services using popular containerized 
+infrastructure-as-code descriptions to a cloud or local
+environment; experiment, measure and evaluate the deployment 
+by injecting faults and adapting the configuration at runtime 
+to test different "what-if" scenarios that reveal the
+limitations of a service before introduced to the public.
+
+Question: {question}
+
+Answer: """
+
+prompt = PromptTemplate(input_variables=['question'], template=template)
+repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+llm = HuggingFaceHub(
+    repo_id=repo_id, model_kwargs={"temperature": 1.0, "max_length": 500}
+)
+
+llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
+
 def read_list():
     # for reading also binary mode is important
     with open('/data/urlcontent.pickle', 'rb') as fp:
@@ -24,83 +49,22 @@ def read_and_return_DB():
     vector_db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)  
     return vector_db
 
-
-
-
-# with warnings.catch_warnings():
-    
-    
 def get_relevant_context (user_input,vector_db):None
+
 def setup():
+    
     question = "What is fogify?"
 
-    template = """
-    Some information about the fogify tool: 
-    Fogify is an emulation Framework easing the modeling, deployment 
-    and experimentation of fog testbeds. 
-    Fogify provides a toolset to: model complex 
-    fog topologies comprised of heterogeneous resources,
-    network capabilities and QoS criteria; deploy the modelled
-    configuration and services using popular containerized 
-    infrastructure-as-code descriptions to a cloud or local
-    environment; experiment, measure and evaluate the deployment 
-    by injecting faults and adapting the configuration at runtime 
-    to test different "what-if" scenarios that reveal the
-    limitations of a service before introduced to the public.
+    response = llm_chain.run({'question': question})
+    return response
 
-    Question: {question}
-
-    Answer: """
-
-    prompt = PromptTemplate(input_variables=[ 'question'],template=template)
-    repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
-    llm = HuggingFaceHub(
-        repo_id=repo_id, model_kwargs={"temperature": 1.0, "max_length":500}
-    )
-
-    llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
-
-    print(llm_chain.run({ 'question':question}))
-# def create_prompt(question):
-#     question = "What is fogify?"
-#     #set up the template of the prompt
-#     template = """
-#     Some information about the fogify tool: 
-#     Fogify is an emulation Framework easing the modeling, deployment 
-#     and experimentation of fog testbeds. 
-#     Fogify provides a toolset to: model complex 
-#     fog topologies comprised of heterogeneous resources,
-#     network capabilities and QoS criteria; deploy the modelled
-#     configuration and services using popular containerized 
-#     infrastructure-as-code descriptions to a cloud or local
-#     environment; experiment, measure and evaluate the deployment 
-#     by injecting faults and adapting the configuration at runtime 
-#     to test different "what-if" scenarios that reveal the
-#     limitations of a service before introduced to the public.
-    
-#     Question: {question}
-    
-#     Answer: """
-#     #add the variables of the prompt
-#     prompt = PromptTemplate(input_variables=[ 'question'],template=template)
-#     repo_id = "gpt2"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
-#     llm = HuggingFaceHub(
-#         repo_id=repo_id, model_kwargs={"temperature": 1.0, "max_length":500}
-#     )
-    
-#     #create the llm chain 
-#     llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
-#     #run the prompt by providing values fort the variables
-#     print( llm_chain.run({'question':question}))
-#     return llm_chain.run({'question':question})
-    
 def main():
     print("============\nFogify chatbot\n============")
     print("Fogify Chatbot: Hello, how can i help. You can type \"exit\" to exit the chat or ask any question about Fogify")
     
     #read database
     vector_db=read_and_return_DB()
-    warnings.simplefilter("ignore")
+    #warnings.simplefilter("ignore")
     while True:
         user_input = input("User: ")
         if user_input.lower() == 'exit':
@@ -108,7 +72,9 @@ def main():
         # context = get_relevant_context(user_input, vector_db)
         # response = get_openai_response(user_input, context)
         # response=create_prompt(user_input)
-        setup()
+        response = setup()
+        # print response type
+        print(response)
         # print("Fogify Chatbot:", response)
 
 if __name__ == "__main__":
